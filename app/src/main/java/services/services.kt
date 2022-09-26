24c333767bf9f0863003584e11cdd64372f78616
arjun-ingole <arjuningole38@gmail.com>
2022-09-26 18:32:43 +0530

    service: inline response variable
    Signed-off-by: arjun-ingole <arjuningole38@gmail.com>

--- app/src/main/java/services/services.kt
+++ app/src/main/java/services/services.kt
@@ -5,14 +5,13 @@ import com.aallam.openai.client.OpenAI
 val apiKey = "API_KEY_HERE"
 val openAI = OpenAI(apiKey)
 
-suspend fun generateResponse(user_prompt :String, category :String): String {
+suspend fun generateResponse(user_prompt: String, category: String): String {
     val request = CompletionRequest(
         model = ModelId("davinci-instruct-beta-v3"),
         prompt = "Generate a $category story on the following prompt, $user_prompt",
         temperature = 0.5,
         topP = 1.0,
     )
-    val response = openAI.completion(request).choices[0].text
-    return response
+    return openAI.completion(request).choices[0].text
 }
 
