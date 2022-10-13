4ae0929d395d742cea1aa823f58271f0a9faa809
Arjun Ingole <arjuningole38@gmail.com>
2022-10-13 14:19:11 +0530

    services: setup coroutine to generate reponses
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- app/src/main/java/services/services.kt
+++ app/src/main/java/services/services.kt
@@ -1,9 +1,11 @@
 import com.aallam.openai.api.completion.CompletionRequest
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
+import kotlinx.coroutines.*
 
 val apiKey = "API_KEY_HERE"
 val openAI = OpenAI(apiKey)
+var story :String = ""
 
 suspend fun generateResponse(user_prompt: String, category: String): String {
     val request = CompletionRequest(
@@ -15,3 +17,8 @@ suspend fun generateResponse(user_prompt: String, category: String): String {
     return openAI.completion(request).choices[0].text
 }
 
+fun getResponse(user_prompt: String, category: String){
+    runBlocking{
+        story = generateResponse(user_prompt, category)
+    }
+}
