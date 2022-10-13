aab74f6853948f196b83db123f1287437417ed3e
Arjun Ingole <arjuningole38@gmail.com>
2022-10-13 18:34:26 +0530

    services: Add parameters to AI and inline story var
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- app/src/main/java/com/gdsc/athena/services/services.kt
+++ app/src/main/java/com/gdsc/athena/services/services.kt
@@ -5,20 +5,26 @@ import kotlinx.coroutines.*
 
 val apiKey = "API_KEY_HERE"
 val openAI = OpenAI(apiKey)
-var story :String = ""
 
-suspend fun generateResponse(user_prompt: String, category: String): String {
+suspend fun generateResponse(user_prompt: String, category: String): String{
     val request = CompletionRequest(
         model = ModelId("davinci-instruct-beta-v3"),
-        prompt = "Generate a $category story on the following prompt, $user_prompt",
-        temperature = 0.5,
+        maxTokens = 2000,
+        presencePenalty = 1.0,
+        n = 1,
+        prompt = "Generate a very long $category story on the following prompt, $user_prompt",
+        temperature = 0.0,
         topP = 1.0,
     )
     return openAI.completion(request).choices[0].text
 }
 
-fun getResponse(user_prompt: String, category: String){
-    runBlocking{
-        story = generateResponse(user_prompt, category)
+fun getResponse(user_prompt: String, category: String) :String{
+    var story = ""
+    runBlocking {
+        launch {
+            story = generateResponse(user_prompt, category)
+        }
     }
+    return story
 }
